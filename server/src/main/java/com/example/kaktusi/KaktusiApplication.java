ada45944563587d4f476d3e7e449d914db154b2b
stiperimac <sr53611@fer.hr>
2023-11-04 11:51:41 +0100

    backend

---
+++ server/src/main/java/com/example/kaktusi/KaktusiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaktusi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaktusiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaktusiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/kaktusi/KaktusiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaktusi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaktusiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
