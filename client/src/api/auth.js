e117b060791d6bc99613ab88bdba139ce916f500
Jakov <sikiric.jakov@outlook.com>
2023-11-04 15:29:15 +0100

    delete unneceesrasry

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,24 +1,24 @@
-import { extractData } from './helpers';
-import request from './request';
+import { extractData } from "./helpers";
+import request from "./request";
 
 const urls = {
-  root: '/users',
+  root: "/users",
   get login() {
-    return this.root + '/login';
+    return this.root + "/login";
   },
   get register() {
-    return this.root + '/register';
+    return this.root + "/register";
   },
   get logout() {
-    return this.root + '/logout';
+    return this.root + "/logout";
   },
 };
 
-const login = credentials => {
+const login = (credentials) => {
   return request.post(urls.login, credentials).then(extractData);
 };
 
-const register = user => {
+const register = (user) => {
   return request.post(urls.register, user).then(extractData);
 };
 
