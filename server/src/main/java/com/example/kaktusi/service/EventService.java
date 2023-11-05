28c1de17457a74aa831948a80c37b53062c875aa
stiperimac <sr53611@fer.hr>
2023-11-05 04:25:45 +0100

    log repository

--- server/src/main/java/com/example/kaktusi/service/EventService.java
+++ server/src/main/java/com/example/kaktusi/service/EventService.java
@@ -2,17 +2,26 @@ package com.example.kaktusi.service;
 
 import com.azure.messaging.eventhubs.EventHubClientBuilder;
 import com.azure.messaging.eventhubs.EventHubConsumerAsyncClient;
+import com.example.kaktusi.entity.OccupyLog;
+import com.example.kaktusi.repository.OccupyLogRepository;
 import com.example.kaktusi.repository.ParkingSpotRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class EventService {
     private final ParkingSpotRepository parkingSpotRepository;
+    private final OccupyLogRepository occupyLogRepository;
     private EventHubConsumerAsyncClient consumerClient;
 
-    public EventService(ParkingSpotRepository parkingSpotRepository) {
+    public EventService(ParkingSpotRepository parkingSpotRepository, OccupyLogRepository occupyLogRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+        this.occupyLogRepository = occupyLogRepository;
     }
 
     public void handleEvent(String id, boolean occupy, String time) {
@@ -21,6 +30,12 @@ public class EventService {
         } else {
             parkingSpotRepository.freeSpot(id);
         }
+        OccupyLog occupyLog = new OccupyLog();
+        occupyLog.setParkingSpotId(id);
+        LocalTime localTime = LocalTime.parse(time);
+        occupyLog.setTimestamp(localTime);
+        occupyLog.setOccupied(occupy);
+        occupyLogRepository.save(occupyLog);
     }
     @Scheduled(fixedRate = 1000)
     public void startEventProcessing() {
@@ -40,10 +55,31 @@ public class EventService {
             String id = event.substring(event.indexOf("Id") + 5, event.indexOf("IsOccupied") - 3);
             String isOccupiedStr = event.substring(event.indexOf("IsOccupied") + 12, event.indexOf("Time") - 2);
             boolean isOccupied = Boolean.parseBoolean(isOccupiedStr);
-            String timeStr = event.substring(event.indexOf("Time") + 7, event.indexOf("Time") + 12);
-            handleEvent(id, isOccupied, timeStr);
+            String timeValue= "";
+            try {
+                // Create an ObjectMapper
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                // Parse the JSON string
+                JsonNode jsonNode = objectMapper.readTree(event);
+
+                // Extract the "Time" value as a string
+                timeValue = jsonNode.get("Time").asText();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(timeValue.length() == 3) {
+                timeValue = "0" + timeValue;
+                timeValue = timeValue.substring(0, 3) + "0" +  timeValue.substring(3);
+            } else if(timeValue.length() == 4 && timeValue.charAt(1) == ':') {
+                timeValue = "0" + timeValue;
+            } else if (timeValue.length() == 4) {
+                timeValue = timeValue.substring(0, 3) + "0" + timeValue.substring(3);
+            }
+
+            handleEvent(id, isOccupied, timeValue);
             System.out.println("Received event: " + eventData.getData().getBodyAsString());
-        });
+           });
 
         // Keep the application running to receive events
         try {
